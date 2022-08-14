894d4df625c7725f56159b10681d2fb01ad33184
Eva Anderson <evajoyanderson@gmail.com>
2022-08-14 12:17:22 +1200

    rough txt file mainpulation

--- GUI.py
+++ GUI.py
@@ -12,21 +12,16 @@ from pygame.locals import(
 
 class display:
     def __init__(self):
-        print("place holder")
+        print()
 
     def start(self):
         pygame.init()
         running = True
         display_surface = pygame.display.set_mode((400, 400 ))
-        card = pygame.image.load("/Users/eva/Desktop/cardsDT2022/card_images/2C.png")
         while running:
             for event in pygame.event.get():
                 if(event.type == WINDOWCLOSE or (event.type == KEYDOWN and event.key == K_ESCAPE)):
                     running = False
             screen = pygame.display.set_mode([800,800])
             screen.fill((255,255,255))
-            screen.blit(card,(1,1))
-            pygame.display.flip()
         pygame.quit()   
-        
-        
