b00885141bb14d7acec37b8242333c2e2de6195c
Eva Anderson <evajoyanderson@gmail.com>
2022-08-29 11:19:18 +1200

    deck display malfuction

--- shuffle.py
+++ shuffle.py
@@ -1,5 +1,6 @@
 import random
-
+import pygame
+from display import cards
 class Game:
     def __init__(self,game):
         self.game = game
@@ -12,7 +13,8 @@ class Arrange:
         with open(game+'.txt') as f:
             contents = f.read()
             shift = ([contents[i:i+2] for i in range(-1, len(contents), 2)])
-            shift.pop(0)
+            shift.pop(0) #try deck = shift.pop(0)
+            # strArray = contents.split()
             Arrange.shuffle(shift)
             
     def shuffle(deck):
@@ -22,9 +24,19 @@ class Arrange:
         
 class Deal:
     def hand(deck,players):
+        ypos = 200
         for x in range(players):
-            hand = (deck[((x-1)*13):(x*13)])
+            hand = (deck[((x-1)*13):(x*13)]) #note this range is only applicable to 4 peoople 
+            x=0
+            for i in hand:
+                if x == 0:
+                    ypos = ypos + 80
+                card = i
+                x = x+1
+                screen = pygame.display.set_mode([800,800])
+                cards(screen,card,[(50*x),ypos])
             
             
 ask = Game(input('')) #this is a temp replacment of another input
 Game.decison('','') #this will need to be called in main file
+
