b826b6a2850a1fabebca558b44bdfbd9bf40ad35
Eva Anderson <evajoyanderson@gmail.com>
2022-10-01 17:22:41 +1300

    final commit

--- display.py
+++ display.py
@@ -1,38 +1,36 @@
 import pygame
-# from information import Label
 from pathlib import Path
+import sys
+import os
 
 
-    
-
-
-
-class cards(pygame.sprite.Sprite):
-    def __init__(self,screen,card,position):
+class Cards(pygame.sprite.Sprite):  # class for displaying cards
+    def __init__(self, screen, card, position):
         pygame.sprite.Sprite.__init__(self)
-        file = (card+'.png')
-        path = ('/Users/eva/Desktop/cardsDT2022/card_images/'+file)
-        if Path(path).is_file():
+        file = card + ".png"  # makes card value image path
+        path = "/Users/eva/Desktop/cardsDT2022/card_images/" + file #replace path for machine
+        if Path(path).is_file():  # if path exists continuie
             pass
         else:
-            print ("Special Card Input Error")
-            exit()
-        image = pygame.image.load(path)
+            # if path doesnt exist reestart program
+            print("Special Card Input Error")
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        image = pygame.image.load(path)  # loads image with path
         self.rect = image.get_rect()
         self.rect.left, self.rect.top = position
         xpos = self.rect.left
         ypos = self.rect.top
-        screen.blit(image,self.rect)
-        
+        screen.blit(image, self.rect)  # renders image w/ position
 
-class instructions(pygame.sprite.Sprite):
-    def __init__(self, screen,language,position):
+
+class Instructions(pygame.sprite.Sprite):
+    def __init__(self, screen, language, position):
         pygame.sprite.Sprite.__init__(self)
-        file = (language+'.png')
-        path = ('/Users/eva/Desktop/cardsDT2022/'+file)
-        image = pygame.image.load(path)
+        file = language + ".png"  # makes language image path
+        path = "/Users/eva/Desktop/cardsDT2022/" + file # replace path for machine
+        image = pygame.image.load(path)  # loads image
         self.rect = image.get_rect()
         self.rect.left, self.rect.top = position
         xpos = self.rect.left
         ypos = self.rect.top
-        screen.blit(image,self.rect)
+        screen.blit(image, self.rect)  # renders image at position
